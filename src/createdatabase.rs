f6d3bdbeafa0f1ecca6db738b516e6720f4df731
Kyle Baumgarten <baumdev@proton.me>
2023-04-19 18:50:13 -0400

    Commiting

--- src/createdatabase.rs
+++ src/createdatabase.rs
@@ -4,7 +4,7 @@ use csv::Reader;
 
 //create a mysql database from a csv file
 pub fn create_database(database_name: &str) {
-    let mut conn = mysql::Conn::new("mysql://root:kcb@localhost").unwrap();
+    let mut conn = mysql::Conn::new("mysql://kylelocal:kcb@localhost:3306").unwrap();
     let query = format!("CREATE DATABASE IF NOT EXISTS {}", database_name);
     conn.query_drop(query).unwrap();
 }
