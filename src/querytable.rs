67d71cb90e3b6de8edc7fee2bb9617245e1b9974
Kyle Baumgarten <baumdev@proton.me>
2023-04-23 21:54:56 -0400

    Committing for real

--- src/querytable.rs
+++ src/querytable.rs
@@ -2,7 +2,7 @@ use mysql::prelude::*;
 use mysql::*;
 use crate::pushdata::gettablecol;
 pub mod displayquery;
-pub fn query_tables(table: &str, conn: &mut PooledConn, whereclause: &str, database: &str)-> Vec<Vec<String>>{
+pub fn query_tables(table: &str, conn: &mut PooledConn, whereclause: &str, database: &str)->Vec<String>{
     let mut query= String::from("SELECT * FROM ");
     query.push_str(table);
     let columns = gettablecol::get_table_col(conn,table, database).unwrap();
@@ -30,28 +30,28 @@ fn grab_columntypes(conn: &mut PooledConn, table: &str, database: &str) -> std::
     Ok(stmt)
 }
 
-fn query_table(conn: &mut PooledConn, table: &str, whereclause: &str, database: &str, columntypes: Vec<String>) -> std::result::Result<Vec<Vec<String>>, Box<dyn std::error::Error>> {
+fn query_table(conn: &mut PooledConn, table: &str, whereclause: &str, database: &str, columntypes: Vec<String>) -> std::result::Result<Vec<String>, Box<dyn std::error::Error>> {
     let mut query= String::from("SELECT * FROM ");
     query.push_str(table);
     if whereclause != "" {
         query.push_str(" WHERE ");
         query.push_str(whereclause);
     }
-    let stmt: Vec<Vec<String>> = conn.query_map(query, |(col1, col2, col3, col4, col5, col6, col7, col8, col9, col10)|{
-        let mut row: Vec<String> = Vec::new();
-        row.push(col1);
-        row.push(col2);
-        row.push(col3);
-        row.push(col4);
-        row.push(col5);
-        row.push(col6);
-        row.push(col7);
-        row.push(col8);
-        row.push(col9);
-        row.push(col10);
-        row
-    })?; //??
+   // let stmt: Vec<Vec<String>> = conn.query_map(query, |(col1, col2, col3, col4, col5, col6, col7, col8, col9, col10)|{
+   //     let mut row: Vec<String> = Vec::new();
+   //     row.push(col1);
+   //     row.push(col2);
+   //     row.push(col3);
+   //     row.push(col4);
+   //     row.push(col5);
+   //     row.push(col6);
+   //     row.push(col7);
+   //     row.push(col8);
+   //     row.push(col9);
+   //     row.push(col10);
+   //     row
+   // })?; //??
 
-    //let stmt:Vec<Result<Vec<String>>>  = conn.query_opt(query).unwrap();
+    let stmt  = conn.query(query).unwrap();
     Ok(stmt)
 }
