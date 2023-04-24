1230a652f5f1bc461c351d154f328ac91ee4967a
Kyle Baumgarten <baumdev@proton.me>
2023-04-24 01:14:51 -0400

    How are we doing

--- src/querytable/displayquery.rs
+++ src/querytable/displayquery.rs
@@ -2,8 +2,8 @@
 pub fn buildhtml(querydata: Vec<Vec<String>>, database:&str, table:&str, columns: Vec<String>)->String{
     let mut html = String::new();
     html.push_str("<html><head><title>Query Results</title>");
-    html.push_str("<style>table,  th { display: block; border: 1px solid black; }
- body{background-color: linen}</style>");
+    html.push_str("<style>table, tr { float: left; } th { display: block; border: 1px solid black; }
+td { border: 1px solid black;} body{background-color: linen}</style>");
 
     html.push_str("</head><body>");
     html.push_str("<h1>");
