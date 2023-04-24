eaf4fcbf8c8b7f469d69cca53cb106374f3f0ec4
Kyle Baumgarten <baumdev@proton.me>
2023-04-23 22:21:08 -0400

    Magic question mark

--- src/querytable.rs
+++ src/querytable.rs
@@ -52,7 +52,7 @@ fn query_table(conn: &mut PooledConn, table: &str, whereclause: &str, database: 
 //        row
 //    })?; //??
     let mut stmt=Vec::new();
-    stmt.push(conn.query_map(query, |(columns)|columns).unwrap()); //
+    stmt.push(conn.query_map(query, |(columns)|columns)?); //
     //let stmt  = conn.query(query).unwrap();
     Ok(stmt)
 }
