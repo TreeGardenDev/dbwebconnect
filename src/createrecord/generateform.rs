4fa17f7dfa2354fd1c63cb81a0f2836d7a544027
Kyle Baumgarten <baumdev@proton.me>
2023-04-26 21:52:31 -0400

    Save form generation

--- src/createrecord/generateform.rs
+++ src/createrecord/generateform.rs
@@ -13,7 +13,7 @@ pub fn buildform(database:&str, table:&str, columns: Vec<String>)->String{
     html.push_str(".");
     html.push_str(table);
     html.push_str("</h1>");
-    html.push_str("<form action='/insert' method='post'>");
+    html.push_str("<form action='/saveform' method='post'>");
     for i in 0..columns.len(){
         html.push_str("<label for='");
         html.push_str(&columns[i]);
