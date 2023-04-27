89589e25624563512ac59fca8be251d790a8ea2b
Kyle Baumgarten <baumdev@proton.me>
2023-04-27 00:08:00 -0400

    Extracting?

--- src/main.rs
+++ src/main.rs
@@ -94,7 +94,7 @@ async fn create(form: web::Form<NewCsv>)-> impl Responder{
     //let _=createrecord::create_record(&mut connection, &form.table.to_string(), &form.database.to_string(), &form.records);
     let html =createrecord::generateform::buildform(database, tablename, columns);
     HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
+        .content_type("application/x-www-form-urlencoded; charset=utf-8")
         .body(html)
 }
 async fn saveform(web::Form(form): web::Form<NewRecord>)-> impl Responder{
