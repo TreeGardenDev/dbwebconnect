ff5c508ff4d3f9591df4a1214aadf44e22b455b1
Kyle Baumgarten <baumdev@proton.me>
2023-05-07 21:50:02 -0400

    CSS SOME MORE

--- src/querytable/displayquery.rs
+++ src/querytable/displayquery.rs
@@ -2,8 +2,39 @@
 pub fn buildhtml(querydata: Vec<Vec<String>>, database:&str, table:&str, columns: Vec<String>)->String{
     let mut html = String::new();
     html.push_str("<html><head><title>Query Results</title>");
-    html.push_str("<style>table, tr { float: left; } th { display: block; border: 1px solid black; }
-td { border: 1px solid black; padding: 15px;} body{background-color: linen}</style>");
+    html.push_str("<style>
+        body {
+    background-color: lightblue;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+    font-size: 14px;
+    color: #666666;
+    -webkit-font-smoothing: antialiased;
+    -webkit-text-size-adjust: 100%;
+    -ms-text-size-adjust: 100%;
+    text-size-adjust: 100%;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    text-align: center;
+}
+form {
+    background: #fff;
+    padding: 40px;
+    max-width: 600px;
+    margin: 40px auto;
+    border-radius: 4px;
+    box-shadow: 0 4px 10px 4px rgba(19, 35, 47, 0.3);
+} 
+input {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+}
+    </style>");
 
     html.push_str("</head><body>");
     html.push_str("<h1>");
