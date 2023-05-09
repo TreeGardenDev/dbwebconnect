9a0656fbc6fb995159a76b2c08b2d8bb694eb375
Kyle Baumgarten <baumdev@proton.me>
2023-05-09 00:12:55 -0400

    Creating db, therefor no db

--- src/createdatabase.rs
+++ src/createdatabase.rs
@@ -3,7 +3,7 @@ use crate::dbconnect;
 //create a mysql database from a csv file
 pub fn create_database(database_name: &str) {
     //let mut conn = mysql::Conn::new("mysql://kylelocal:kcb@127.0.0.1:3306/").unwrap();
-    let mut conn=dbconnect::database_connection(&database_name);
+    let mut conn=dbconnect::database_connection_no_db();
     let query = format!("CREATE DATABASE IF NOT EXISTS {}", database_name);
     conn.query_drop(query).unwrap();
 }
