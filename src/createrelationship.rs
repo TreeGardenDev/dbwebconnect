fcdc1e5bff006b1ff28d1eecfd3c2c22889f5248
Kyle Baumgarten <BaumDev@proton.me>
2023-05-12 21:30:04 -0400

    Progress

---
+++ src/createrelationship.rs
@@ -0,0 +1,53 @@
+use mysql::prelude::Queryable;
+
+use crate::Reader;
+use crate::dbconnect;
+
+fn createrelationship(file:String)->String{
+    //read file
+    //create relationship
+    //send to database
+
+   //let conn= dbconnect::database_connection(database);
+    let mut reader = Reader::from_path(file).unwrap();
+    for result in reader.records() {
+        let record = result.unwrap();
+            let mut query:String= String::from("Alter Table ");
+            query.push_str(&record[0]);
+            query.push_str(" Add Constraint ");
+            query.push_str(&record[0]);
+            query.push_str("_");
+            query.push_str(&record[1]);
+            query.push_str("_");
+            query.push_str(&record[2]);
+            query.push_str("_");
+            query.push_str(&record[3]);
+            query.push_str(" Foreign Key (");
+            query.push_str(&record[1]);
+            query.push_str(") References ");
+            query.push_str(&record[2]);
+            query.push_str("(");
+            query.push_str(&record[3]);
+            query.push_str(")");
+            query.push_str(" on Delete ");
+            query.push_str(&record[4]);
+            query.push_str(" on Update ");
+            query.push_str(&record[5]);
+            println!("{}", query);
+            query;
+    }
+    String::from("Unable to create relationship")
+
+}
+
+pub fn commitrelationship(database: &str, file:String){
+
+    let relation=createrelationship(file);
+    let mut conn= dbconnect::database_connection(database);
+    
+    let _= conn.query_drop(relation);
+}
+
+
+
+
